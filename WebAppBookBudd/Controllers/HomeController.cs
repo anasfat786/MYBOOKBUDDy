6cce2e48460b7e1db078a09b804e01143f69bd79
anasfatima562@gmail.com <anasfatima562@gmail.com>
2021-03-31 14:23:23 +0530

    fisrt commit

--- WebAppBookBudd/Controllers/HomeController.cs
+++ WebAppBookBudd/Controllers/HomeController.cs
@@ -20,7 +20,7 @@ namespace WebAppBookBudd.Controllers
 
         public IActionResult Index()
         {
-            return View();
+            return RedirectToAction ("Index","Products" );
         }
 
         public IActionResult Privacy()
